c0ae71b10552ff542119af68e250eb5683852e2f
jamesYoungs4 <jamesyoungs004@gmail.com>
2026-01-20 11:15:43 +0000

    Controller and Model for users made progress

---
+++ backend/controllers/users.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+async function register(req, res) {
+    try {
+        const data = req.body;
+
+        if (!data.username || !data.password || !data.email_address){
+            throw new Error('Invalid user details');
+        }
+
+        // generate salt with specific cost
+        const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
+
+        // hash password
+        data["password"] = await bcrypt.hash(data.password, salt);
+        const result = await User.create(data);
+
+        res.status(201).send(result);
+    } catch (err) {
+        if (err.message.includes('already taken')) {
+            return res.status(409).json({ error: err.message });
+        }
+        res.status(500).json({ error: 'Registration failed:' + err.message });
+    }
+}
+
+async function login(req, res) {
+    try {
+        const data = req.body;
+
+        if (!data.username || !data.password) {
+            throw new Error('Please provide username and password')
+        }
+
+        const user = await User.getOneByUsername(data.username);
+
+        const match = await bcrypt.compare(data.password, user.password)
+        if (!match) {
+            throw new Error('Invalid credentials')
+        }
+
+        const payload = {
+            user_id: user.user_id,
+            username: user.username
+        }
+
+        const sendToken = (err, token) => {
+            if(err) { 
+                throw new Error('Error in token generation')
+            }
+            res.status(200).json({
+                success: true,
+                token: token,
+                user: {username: user.username, email_address: user.email_address}
+            });
+        }
+
+        jwt.sign(payload, process.env.SECRET_TOKEN, { expiresIn: 3600 }, sendToken);
+
+    } catch (err) {
+        res.status(401).json({ error: err.message });
+    }
+}
+
+module.exports = { register, login }
---
+++ backend/models/User.js
@@ -0,0 +1,62 @@
+const db = require('../database/connection');
+
+class User{
+    constructor({user_id, username, password, email_address, spotting_points, achievement_points, total_points, current_pfp, current_title, daily_streak}) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.email_address = email_address;
+        this.spotting_points = spotting_points || 0.0;
+        this.achievement_points = achievement_points || 0;
+        this.total_points = Math.ceil(this.spotting_points) + this.achievement_points;
+        this.current_pfp = current_pfp || '';
+        this.current_title = current_title || '';
+        this.daily_streak = daily_streak || 0;
+    }
+
+    static async getAll(){
+        const response = await db.query("SELECT * FROM users;");
+        return response.rows.map(p => new User(p));
+    }
+
+    static async getOneByID(id){
+        const response = await db.query("SELECT * FROM users WHERE user_id = $1;", [id]);
+        if (response.rows.length != 1){
+            throw new Error("Unable to locate user.");
+        }
+        return new User(response.rows[0]);
+    }
+
+    static async getOneByUsername(name){
+        const response = await db.query("SELECT * FROM users WHERE username = $1;", [name]);
+        if (response.rows.length != 1){
+            throw new Error("Unable to locate user.");
+        }
+        return new User(response.rows[0]);
+    }
+
+    static async create(data){
+        const { username, password, email_address } = data;
+        const emailTaken = await db.query("SELECT * FROM users WHERE email_address = $1;", [email_address]);
+        if (emailTaken.rows.length !== 0) {
+            throw new Error('Email address is already taken.');
+        }
+        const usernameTaken = await db.query("SELECT * FROM users WHERE username = $1;", [username]);
+        if (usernameTaken.rows.lenfth !== 0){
+            throw new Error('Username is already taken.');
+        }
+        let response = await db.query("INSERT INTO users (username, password, email_address) VALUES ($1, $2, $3) RETURNING user_id;", [username, password, email_address]);
+        const new_id = response.rows[0].user_id;
+        const new_user = await User.getOneByID(new_id);
+        return new_user;
+    }
+
+    static async updatePointsByID(id){
+        const spot_response = await db.query("SELECT SUM(spot_points) FROM spottings WHERE user_id = $1;", [id]);
+        const achievement_response = await db.query("SELECT SUM(value) FROM achievements WHERE achievement_id IN (SELECT achievement_id FROM achievement_user_complete WHERE user_id = $1);", [id]);
+        const total_points = Math.ceil(spot_response) + achievement_response;
+        const response = await db.query("UPDATE users SET spotting_points = $1, achievement_points = $2, total_points = $3 WHERE user_id = $4;", [spot_response, achievement_response, total_points, id]);
+    } 
+}
+
+module.exports = User;
