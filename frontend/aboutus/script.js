3295f4792f970224797d836b4a0e9c34031f2802
BenFleck25 <ben.fleck2508@outlook.com>
2026-01-21 14:58:40 +0000

    bug fix submit page, added functionality to all dropdown menus and landing page buttons

---
+++ frontend/aboutus/script.js
@@ -0,0 +1,27 @@
+const homeBtn = document.querySelector("#logo")
+const homeBtn2 = document.querySelector("#title")
+
+homeBtn.addEventListener("click", () => {
+    window.location.href = "../landingpage/index.html"
+})
+homeBtn2.addEventListener("click", () => {
+    window.location.href = "../landingpage/index.html"
+})
+
+const dropDown1 = document.querySelector("#dd1")
+const dropDown2 = document.querySelector("#dd2")
+const dropDown3 = document.querySelector("#dd3")
+const dropDown4 = document.querySelector("#dd4")
+
+dropDown1.addEventListener("click", () => {
+    window.location.href = "../myaccount/index.html"
+})
+dropDown2.addEventListener("click", () => {
+    window.location.href = "../aboutus/index.html"
+})
+dropDown3.addEventListener("click", () => {
+    window.location.href = "../spottedmap/index.html"
+})
+dropDown4.addEventListener("click", () => {
+    window.location.href = "../leaderboards/index.html"
+})
