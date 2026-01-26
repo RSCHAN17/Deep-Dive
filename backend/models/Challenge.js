f5f1ba676c25414c307decfc7d2a15f1107fed3a
jamesYoungs4 <jamesyoungs004@gmail.com>
2026-01-26 10:46:33 +0000

    created challenge model

---
+++ backend/models/Challenge.js
@@ -0,0 +1,9 @@
+const db = require('../database/connection');
+
+class Challenge{
+    constructor({id, challenge_name, challenge_description, points}){
+        this.id = id;
+        this.challenge_name = challenge_name;
+        this.challenge_description
+    }
+}
