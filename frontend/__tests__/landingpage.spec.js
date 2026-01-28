6c3b0fa5f38e57aee5b4d431e847e3558b4f353a
BenFleck25 <ben.fleck2508@outlook.com>
2026-01-28 11:04:37 +0000

    fixed loading issues and achievemnt ticks

---
+++ frontend/__tests__/landingpage.spec.js
@@ -0,0 +1,23 @@
+const { renderDOM } = require('./helpers');
+let dom;
+let document;
+
+describe('index.html', () => {
+
+    beforeEach(async () => {
+        dom = await renderDOM('http://127.0.0.1:5500/frontend/landingpage/index.html');
+        document = await dom.window.document;
+
+
+    })
+    it('has 4 navigations buttons', () => {
+        const dd1 = document.querySelector('#dd1')
+        expect(dd1).toBeTruthy()
+        const dd2 = document.querySelector('#dd2')
+        expect(dd2).toBeTruthy()
+        const dd3 = document.querySelector('#dd3')
+        expect(dd3).toBeTruthy()
+        const dd4 = document.querySelector('#dd4')
+        expect(dd4).toBeTruthy()
+    })
+})
